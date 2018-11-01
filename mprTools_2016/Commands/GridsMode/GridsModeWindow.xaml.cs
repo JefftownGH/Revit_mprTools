11f4e9136e4ef6c5ab84c7a8bf1d41d0473aeddd
Modis <cjmodis@gmail.com>
2018-11-01 21:55:59 +0300

    Add ModPlusStyle

--- mprTools_2016/Commands/GridsMode/GridsModeWindow.xaml.cs
+++ mprTools_2016/Commands/GridsMode/GridsModeWindow.xaml.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows;
-using Autodesk.Revit.DB;
-using Autodesk.Revit.UI;
-using ModPlusAPI.Windows;
-
-namespace mprTools.Commands.GridsMode
+﻿namespace mprTools.Commands.GridsMode
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows;
+    using Autodesk.Revit.DB;
+    using Autodesk.Revit.UI;
+    using ModPlusAPI.Windows;
+
     public partial class GridsModeWindow
     {
         private readonly UIApplication _uiApplication;
